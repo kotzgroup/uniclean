88b5a03b1d8a767e5ddc042a00b611f757a6decb
David Kotz <David.F.Kotz@dartmouth.edu>
2019-11-30 09:53:29 +0100

    script can now serve as a filter from stdin to stdout, when no filenames are given

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 Translate input from Unicode (UTF-8) to LaTeX or ASCII equivalents.
 Each file is ***OVERWRITTEN*** with the updated text, unless no changes are needed.
+If no files are listed, stdin is processed to stdout.
+Untranslated codes are reported as warnings to stderr.
 
 usage:
 
 ```
-   isoclean.py [--xml|--latex] file...
+   isoclean.py [--xml|--latex] [file]...
 ```
 
 where
@@ -15,17 +17,16 @@ where
  * optional `--latex` will translate to LaTeX equivalents;
  * default is plain-text ASCII equivalents.
 
-The latter two may leave some characters unmapped, and warn to stderr;
+The latter two cases may leave some characters unmapped, and warn to stderr;
 if such warnings occur, search the output for `\N{..}` and fix it by hand.
 Then add a new entry to the mapping tables here, to help others in future!
 
+If no files are listed, the stdin is transformed and printed to stdout.
+Otherwise, each file is transformed and written back to the same file; if no changes were needed, the file is not touched.
+
 ## References:
  * [https://docs.python.org/3/howto/unicode.html]()
- * [https://docs.python.org/3/library/stdtypes.html#str.maketrans]()
- * [https://docs.python.org/3/library/stdtypes.html#str.translate]()
- * [https://docs.python.org/3/library/stdtypes.html#str.encode]()
- * [https://docs.python.org/3/library/stdtypes.html#bytes.decode]()
 
 ## Author
-2019 David Kotz kotz@dartmouth.edu
+David Kotz, 2019: <kotz@dartmouth.edu>
 
--- isoclean.py
+++ isoclean.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 # Translate input from Unicode (UTF-8) to LaTeX or ASCII equivalents.
 # Each file is OVERWRITTEN with the updated text, unless no changes are needed.
+# If no files are listed, stdin is processed to stdout.
+# Untranslated codes are reported as warnings to stderr.
 #
-# usage:
-#   isoclean.py [--xml|--latex] file...
+usage = "usage: isoclean.py [--xml|--latex] [file]..."
 # where
 #  optional --xml   will translate to XML (HTML) equivalents;
 #  optional --latex will translate to LaTeX equivalents;
 #  default is plain-text ASCII equivalents.
-# The latter two may leave some characters unmapped, and warn to stderr;
+# The latter two cases may leave some characters unmapped, and warn to stderr;
 # if such warnings occur, search the output for "\N{..}" and fix it by hand.
 # Then add a new entry to the mapping tables here, to help others in future!
+#
+# If no files are listed, the stdin is transformed and printed to stdout.
+# Otherwise, each file is transformed and written back to the same file; if no changes
+# were needed, the file is not touched.
 #
 # References:
 #  https://docs.python.org/3/howto/unicode.html
@@ -18,26 +23,22 @@
 #  https://docs.python.org/3/library/stdtypes.html#str.translate
 #  https://docs.python.org/3/library/stdtypes.html#str.encode
 #  https://docs.python.org/3/library/stdtypes.html#bytes.decode
+#  https://stackoverflow.com/questions/16549332/python-3-how-to-specify-stdin-encoding
 #
 # 2019 David Kotz kotz@dartmouth.edu
 # 
 
 import sys
 
-usage = "usage: isoclean.py [--xml|--latex] file..."
-
-if not sys.argv[1:]:
-    exit(usage)
-
-############# process arguments #####
+############# main: process arguments #####
 
 # set up the mapping table
-if sys.argv[1] == "--xml":
+if sys.argv[1:] and sys.argv[1] == "--xml":
     # XML mappings (also useful for HTML)
     fileList = sys.argv[2:]
     map = { } # we need no map: all non-ASCII will be mapped to '&#xxx;'
     mapErrors = 'xmlcharrefreplace'
-elif sys.argv[1] == "--latex":
+elif sys.argv[1:] and sys.argv[1] == "--latex":
     # latex mappings
     fileList = sys.argv[2:]
     mapErrors = 'namereplace'  # unmapped codes will output names '\N{...}'
@@ -78,39 +79,53 @@ else:
 # either way, build a translation table from that map
 mapTable = str.maketrans(map)
 
-# and verify that there are some filenames provided
+
+################### transform: function to transform Unicode from sInput #############
+# arguments:
+#   sInput is a str that may contain Unicode
+#   source is a str that describes the source of sInput ("stdin" or a filename)
+# return value is a str with all Unicode mapped according to mapTable and mapErrors
+
+def transform(sInput, source):
+    # map characters to ASCII equivalent, according to above mappings
+    sText = sInput.translate(mapTable)
+
+    # convert to ASCII byte-string; handle remaining unmapped chars here
+    bText = sText.encode('ascii', mapErrors)
+    # convert that 'bytes' object back to a 'str' object
+    sText = bText.decode('ascii')
+
+    # warn about unreplaced Unicode characters
+    s = 0 # index to start looking within sText
+    while '\\N{' in sText[s:]:
+        s = sText.find('\\N{', s)
+        e = sText.find('}', s)
+        print(source+": unknown Unicode", sText[s:e+1], file=sys.stderr)
+        s = e
+
+    return sText
+
+
+############# main: process files or stdin #########
+
+# Behavior depends on whether arguments include filenames
 if not fileList:
-    print("No input files specified.")
-    sys.exit(usage)
+    # no files listed - process stdin; assume UTF-8; don't translate line-endings
+    sys.stdin.reconfigure(encoding='utf-8', newline='')
+    sInput = sys.stdin.read()
+    sText = transform(sInput, "stdin")
+    sys.stdout.write(sText)
 
-############# process files #########
-
-# Now scan and convert each file
-for filename in fileList:
-    # open the file in 'text' mode; assume UTF-8; don't translate line-endings
-    with open(filename, mode='rt', encoding='UTF-8', newline='') as fInput:
-        # read all its text as a Unicode string
-        sInput = fInput.read()
-
-        # map characters to ASCII equivalent, according to above mappings
-        sText = sInput.translate(mapTable)
-
-        # convert to ASCII byte-string; handle remaining unmapped chars here
-        bText = sText.encode('ascii', mapErrors)
-        # convert that 'bytes' object back to a 'str' object
-        sText = bText.decode('ascii')
-
-        # warn about unreplaced Unicode characters 
-        s = 0 # index to start looking within sText
-        while '\\N{' in sText[s:]:
-            s = sText.find('\\N{', s)
-            e = sText.find('}', s)
-            print(filename+": unknown Unicode", sText[s:e+1], file=sys.stderr)
-            s = e
-
-        # overwrite the original file with the updated string, if different
-        if sText != sInput:
-            with open(filename, 'wt') as fOutput:
-                fOutput.write(sText)
-
-# end of for loop over all filenames
+else:
+    # one or more files listed - process each in turn
+    for filename in fileList:
+        # open the file in 'text' mode; assume UTF-8; don't translate line-endings
+        with open(filename, mode='rt', encoding='UTF-8', newline='') as fInput:
+            # read all its text as a Unicode string
+            sInput = fInput.read()
+            # transform its Unicode characters
+            sText = transform(sInput, filename)
+            # overwrite the original file with the updated string, if different
+            if sText != sInput:
+                with open(filename, 'wt') as fOutput:
+                    fOutput.write(sText)
